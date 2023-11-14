77f77bc6ec3714082fe336a033aa104af2a7aa3b
USER <hamidhanalauddin789@gmail.com>
2023-11-14 14:46:17 +0700

    custom component

--- lib/pages/home_pages.dart
+++ lib/pages/home_pages.dart
@@ -6,33 +6,47 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            backgroundColor: Colors.green.shade300,
-            title: Text(
-              "Home Page",
-              style: TextStyle(color: Colors.white),
+      appBar: AppBar(
+          backgroundColor: Colors.green.shade300,
+          title: Text(
+            "Home Page",
+            style: TextStyle(color: Colors.white),
+          )),
+      body: Column(
+        children: [
+          coustemmenu(imageAssets: "asset/belah2.png", title: "persegi"),
+          coustemmenu(imageAssets: "asset/lingkaran.png", title: "persegi"),
+        ],
+      ),
+    );
+  }
+}
 
-            )),
-        body: Column(
-    children: [
-    Container(
-        margin: EdgeInsets.all(8),
-    padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-    decoration: BoxDecoration(
-    color: Colors.green
-    ),
-    child: Text("Persegi"),
-    ),
-      Container(
-        margin: EdgeInsets.all(8),
-        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-        decoration: BoxDecoration(
-            color: Colors.green
-        ),
-        child: Text("segitiga"),
-    ),
-    ],
-    ),
+class coustemmenu extends StatelessWidget {
+  const coustemmenu({
+    super.key,
+    required this.imageAssets,
+    required this.title,
+  });
+
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.green),
+      child: Column(
+        children: [
+          Image.asset(
+            imageAssets,
+            height: 100,
+          ),
+          Text(title),
+        ],
+      ),
     );
   }
 }
